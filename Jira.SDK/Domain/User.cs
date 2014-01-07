6763016f80b8b25ebe72458272274acaefe37306
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2014-01-07 17:08:56 +0100

    Added extra fields on User and made it comparable.

--- Jira.SDK/Domain/User.cs
+++ Jira.SDK/Domain/User.cs
@@ -10,9 +10,24 @@ namespace Jira.SDK
     {
         public String Key { get; set; }
         public String Name { get; set; }
+        public String DisplayName { get; set; }
         public String EmailAddress { get; set; }
 
-        public String Username { get { return Key; }}
-        public String Fullname { get { return Name;  }}
+        public String Username { get { return Key ?? Name; } }
+        public String Fullname { get { return DisplayName ?? Name; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is User)
+            {
+                return this.Username.Equals((obj as User).Username);    
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Username.GetHashCode();
+        }
     }
 }
