2b99bf786d29cc788a6b6f4a5a46d09f49eda778
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2014-04-15 09:54:52 +0200

    Made the GetSprints method lazy loaded

--- Jira.SDK/Domain/AgileBoard.cs
+++ Jira.SDK/Domain/AgileBoard.cs
@@ -11,13 +11,17 @@ namespace Jira.SDK.Domain
 		public Int32 ID { get; set; }
 		public String Name { get; set; }
 		public Boolean SprintSupport { get; set; }
-		public JiraEnvironment Environment {get;set;}
+		public JiraEnvironment Environment { get; set; }
 
+		private List<Sprint> _sprints;
 		public List<Sprint> GetSprints()
 		{
-			List<Sprint> sprints = Environment.Client.GetSprintsFromAgileBoard(this.ID);
-			sprints.ForEach(sprint => sprint.Environment = this.Environment);
-			return sprints;
+			if (_sprints == null)
+			{
+				_sprints = Environment.Client.GetSprintsFromAgileBoard(this.ID);
+				_sprints.ForEach(sprint => sprint.Environment = this.Environment);
+			}
+			return _sprints;
 		}
 	}
 }
