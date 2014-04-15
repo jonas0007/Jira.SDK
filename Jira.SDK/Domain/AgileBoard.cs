1e9251b5d180ea814a73c994f33e39e016d60b01
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2014-04-15 08:09:55 +0200

    Added AgileBoard, Sprint and SprintDetail as domain objects.

---
+++ Jira.SDK/Domain/AgileBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.SDK.Domain
+{
+	public class AgileBoard
+	{
+		public Int32 ID { get; set; }
+		public String Name { get; set; }
+		public Boolean SprintSupport { get; set; }
+		public JiraEnvironment Environment {get;set;}
+
+		public List<Sprint> GetSprints()
+		{
+			List<Sprint> sprints = Environment.Client.GetSprintsFromAgileBoard(this.ID);
+			sprints.ForEach(sprint => sprint.Environment = this.Environment);
+			return sprints;
+		}
+	}
+}
---
+++ Jira.SDK/Domain/Sprint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Jira.SDK.Domain
+{
+	public class Sprint
+	{
+		public Int32 ID { get; set; }
+		public String Name { get; set; }
+		public String State { get; set; }
+
+		public JiraEnvironment Environment { get; set; }
+
+		private List<Issue> _issues;
+		public List<Issue> GetIssues()
+		{
+			return _issues ?? (_issues = Environment.Client.GetIssuesFromSprint(this.ID));
+		}
+	}
+}
