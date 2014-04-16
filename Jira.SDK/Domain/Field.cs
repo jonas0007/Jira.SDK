ad7b6ef05167792d7fc4af18c9e00d3cb44b3be9
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2014-04-16 14:31:12 +0200

    Started adding customfield support for issues Improved the loading time for Subtasks

---
+++ Jira.SDK/Domain/Field.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.SDK.Domain
+{
+	public class Field
+	{
+		public String ID { get; set; }
+		public String Name { get; set; }
+		public Boolean Custom { get; set; }
+	}
+}
