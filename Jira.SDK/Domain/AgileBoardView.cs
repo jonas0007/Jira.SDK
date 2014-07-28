110169decf11fd0c69e184c51e224b24a79657eb
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2014-07-28 13:43:31 +0200

    Added missing files which where not checked in due to incorrect sourcetree settings.

---
+++ Jira.SDK/Domain/AgileBoardView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.SDK.Domain
+{
+	public class AgileBoardView
+	{
+		public List<AgileBoard> Views { get; set; }
+	}
+}
---
+++ Jira.SDK/Domain/CustomField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jira.SDK.Domain
+{
+	public class CustomField
+	{
+		public Int32 ID { get; set; }
+		public String Value { get; set; }
+	}
+}
