4ca1c3639f6729e4717e0125bd5980ed0417cd9c
Jonas Verdickt <jonas.verdickt@staff.telenet.be>
2015-01-06 11:26:21 +0100

    Added extra statusses to the status enum

--- Jira.SDK/Domain/Status.cs
+++ Jira.SDK/Domain/Status.cs
@@ -19,12 +19,19 @@ namespace Jira.SDK
 
     public enum StatusEnum
     {
-        Open,
-        InProgress,
-        Reopened,
-        Resolved,
-        Closed,
-		Waitingforinformation,
-		Readyfortesting
+        Open = 0,
+        InProgress = 20,
+        Reopened = 1,
+        Resolved = 30,
+        Closed = 32,
+		Waitingforinformation = 10,
+		Readyfortesting = 31,
+		InReview = 32,
+		InDevelopment = 21,
+		Analysing = 11,
+		ReadyforRelease = 31,
+		Waitingforapproval = 2,
+		AnalysingbyQA = 12,
+		Approved = 3,
     }
 }
