8f6f81a6b57cd558116d9a74c7dcd7c96ea78a83
jonas0007 <verdickt.jonas@staff.telenet.be>
2015-06-15 12:04:08 +0200

    - Updated copyright line in the AssemblyInfo

--- Jira.SDK/Properties/AssemblyInfo.cs
+++ Jira.SDK/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("jonas0007")]
 [assembly: AssemblyProduct("Jira.SDK")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
