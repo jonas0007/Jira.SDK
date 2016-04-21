c791ec27eae97f7fa16498d4afd7c8d7f2d2d127
Ari Patrick <ari@aripatrick.com>
2016-04-21 11:58:18 -0500

    Formatting fixes

--- Jira.SDK/Domain/IssueFilter.cs
+++ Jira.SDK/Domain/IssueFilter.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace Jira.SDK.Domain {
     public class IssueFilter {
         private Jira _jira { get; set; }
-        public Jira GetJira() {
+        public Jira GetJira()
+        {
             return _jira;
         }
 
-        public void SetJira(Jira jira) {
+        public void SetJira(Jira jira)
+        {
             _jira = jira;
         }
 
@@ -20,8 +22,10 @@ namespace Jira.SDK.Domain {
         public String JQL { get; set; }
 
         private List<Issue> _issues;
-        public List<Issue> GetIssues(Int32 maxResults = 700) {
-            if (_issues == null) {
+        public List<Issue> GetIssues(Int32 maxResults = 700)
+            {
+            if (_issues == null)
+            {
                 _issues = _jira.Client.SearchIssues(this.JQL, maxResults);
                 _issues.ForEach(issue => issue.SetJira(this._jira));
             }
