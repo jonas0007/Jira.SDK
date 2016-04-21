ae34f273abc11584ac3f5e7b8940f61ce79c463e
Ari Patrick <ari@aripatrick.com>
2016-04-21 11:49:53 -0500

    maxResults configurable fixes #15

--- Jira.SDK/Domain/IssueFilter.cs
+++ Jira.SDK/Domain/IssueFilter.cs
@@ -4,34 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Jira.SDK.Domain
-{
-    public class IssueFilter
-    {
-		private Jira _jira { get; set; }
-		public Jira GetJira()
-		{
-			return _jira;
-		}
+namespace Jira.SDK.Domain {
+    public class IssueFilter {
+        private Jira _jira { get; set; }
+        public Jira GetJira() {
+            return _jira;
+        }
 
-		public void SetJira(Jira jira)
-		{
-			_jira = jira;
-		}
+        public void SetJira(Jira jira) {
+            _jira = jira;
+        }
 
         public String Name { get; set; }
         public String Description { get; set; }
         public String JQL { get; set; }
 
         private List<Issue> _issues;
-        public List<Issue> GetIssues()
-        {
-                if (_issues == null)
-                {
-                    _issues = _jira.Client.SearchIssues(this.JQL);
-                    _issues.ForEach(issue => issue.SetJira(this._jira));
-                }
-                return _issues;
+        public List<Issue> GetIssues(Int32 maxResults = 700) {
+            if (_issues == null) {
+                _issues = _jira.Client.SearchIssues(this.JQL, maxResults);
+                _issues.ForEach(issue => issue.SetJira(this._jira));
+            }
+            return _issues;
         }
     }
 }
